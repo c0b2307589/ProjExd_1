51af9a8f41463a629201a8f792ca6b32fee82560
c0b2307589 <c0b2307589@edu.teu.ac.jp>
2024-11-19 17:22:17 +0900

    演習1

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -15,29 +15,23 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 400, 300
     tmr = 0
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        screen.blit(bg_img, [0, 0])
         key_lst = pg.key.get_pressed()
-        move_x, move_y = 0, 0
         if key_lst[pg.K_UP]:
-            move_y -= 1
+            kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
-            move_y += 1
+            kk_rct.move_ip((0, 1))
         if key_lst[pg.K_RIGHT]:
-            move_x += 2
+            kk_rct.move_ip((1, 0))
         if key_lst[pg.K_LEFT]:
-            move_x -= 2
+            kk_rct.move_ip((-1, 1))
         else:
-            move_x -= 1  # 何も押されていない場合は左に流れる
-
-        kk_rct.move_ip(move_x, move_y)
-
-
-        x = -(tmr%3200)
-    
+            kk_rct.move_ip((-1,0))  # 何も押されていない場合は左に流れる
+        x = -(tmr%3200) #練習6-2
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
@@ -45,10 +39,11 @@ def main():
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
+        clock.tick(10)
         clock.tick(200)
+
 
 if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
-    sys.exit()
