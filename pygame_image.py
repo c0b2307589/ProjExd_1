5a415a4a99428e1e653d6ebbc8f164c56932ed32
c0b2307589 <c0b2307589@edu.teu.ac.jp>
2024-11-19 16:35:23 +0900

    上下左右に飛行できるようになった

--- pygame_image.py
+++ pygame_image.py
@@ -4,21 +4,38 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像Surfaceを作成する
-    kk_img = pg.image.load("fig/3.png")  # こうかとん画像Surafaceを作成する
-    kk_img = pg.transform.flip(kk_img, True, False)  # こうかとんを左右反転させる
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()#こうかとんのrectを取得
+    kk_rct.center = 400, 300
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])  # screen Surfaceに背景画像Surfaceを貼り付ける
-        screen.blit(kk_img, [300, 200])  # screen Surfaceにこうかとん画像Surfaceを貼り付ける
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1) 
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,1)
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1,0)
+        x = -(tmr%3200) #練習6-2
+
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
